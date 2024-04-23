338d5c864d6c6062947c5ced3876960187ef47cb
Kaique Torres <kaique.serrot@gmail.com>
2024-04-23 10:14:15 -0500

    Initial Commit

---
+++ pChart/pChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  pChartApp.swift
+//  pChart
+//
+//  Created by Kaique Torres on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct pChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
